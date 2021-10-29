1153f7a30f453068f552fdbbe90ce8a4fcc94d62
z9877969 <z9877969@ukr.net>
2021-10-29 18:29:22 +0300

    adds methods addTransaction, handleSubmit

--- src/components/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesList/CategoriesList.jsx
@@ -1,6 +1,12 @@
-const CategoriesList = () => {
+import PropTypes from "prop-types";
+
+const CategoriesList = ({ title }) => {
   console.log("CategoriesList");
-  return <h1>CategoriesList</h1>;
+  return <h1>{title}</h1>;
+};
+
+CategoriesList.propTypes = {
+  title: PropTypes.string,
 };
 
 export default CategoriesList;
