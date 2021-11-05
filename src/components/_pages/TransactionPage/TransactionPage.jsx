1af499cf1127529f11a656c779aa089917df59fe
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-11-05 15:29:53 +0200

    adds hooks to transaction page

--- src/components/_pages/TransactionPage/TransactionPage.jsx
+++ src/components/_pages/TransactionPage/TransactionPage.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import PropTypes from "prop-types";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
@@ -12,181 +12,171 @@ import {
 } from "../../../assets/options/transactionCategories.json";
 import { addTransactionApi } from "../../../services/api";
 
+const TransactionPage = ({
+  transType,
+  handleGoBack,
+  addTransaction,
+  setError,
+}) => {
+  const [day, setDay] = useState("2021-10-29");
+  const [time, setTime] = useState("16:15");
+  const [category, setCategory] = useState(
+    transType === "incomes" ? "salary" : "foods"
+  );
+  const [sum, setSum] = useState("");
+  const [currency, setCurrency] = useState("UAH");
+  const [comment, setComment] = useState("");
+  const [isOpenCatList, setIsOpenCatList] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [categoriesList, setCategoriesList] = useState(
+    transType === "costs" ? costsCategories : incomesCategories
+  );
 
-class TransactionPage extends Component {
-  static propTypes = {
-    transType: PropTypes.string.isRequired,
-    handleGoBack: PropTypes.func.isRequired,
-    addTransaction: PropTypes.func.isRequired,
-  };
+  const toggleCatList = () => setIsOpenCatList((prev) => !prev);
 
-  state = {
-    day: "2021-10-29",
-    time: "16:15",
-    category: this.props.transType === "incomes" ? "salary" : "foods",
-    sum: "",
-    currency: "UAH",
-    comment: "",
-    isOpenCatlList: false,
-    isLoading: false,
-    categoriesList:
-      this.props.transType === "costs" ? costsCategories : incomesCategories,
-  };
-
-  toggleCatList = () => {
-    const cbForSetState = (prevState) => ({
-      isOpenCatlList: !prevState.isOpenCatlList,
-    });
-    this.setState(cbForSetState);
-  };
-
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
-    name === "category" && this.setState({ isOpenCatlList: false });
+
+    switch (name) {
+      case "day":
+        setDay(value);
+        return;
+      case "time":
+        setTime(value);
+        return;
+      case "category":
+        setCategory(value);
+        setIsOpenCatList(false);
+        return;
+      case "sum":
+        setSum(value);
+        return;
+      case "currency":
+        setCurrency(value);
+        return;
+      case "comment":
+        setComment(value);
+        return;
+      default:
+        return;
+    }
   };
 
-  // openCatList = () => {
-  //   this.setState({ isOpenCatlList: true });
-  // };
+  const toggleLoader = () => {
+    setIsLoading((prev) => !prev);
+  };
 
-  // closeCatList = () => {
-  //   this.setState({ isOpenCatlList: false });
-  // };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { isOpenCatlList, categoriesList, isLoading, ...dataForm } =
-      this.state;
-    const { addTransaction, transType, setError } = this.props;
-    this.toggleLoader();
-    addTransactionApi({ transaction: dataForm, transType })
-      .then((transaction) => addTransaction({ transaction, transType }))
-      .catch((err) => setError(err.message))
-      .finally(() => this.toggleLoader());
-  };
-
-  toggleLoader = () => {
-    this.setState({ isLoading: !this.state.isLoading });
-  };
-
-  deleteCategory = (id) => {
-    console.log(id);
-    this.setState((prevState) => {
-      return {
-        categoriesList: prevState.categoriesList.filter(
-          (category) => category.name !== id
-        ),
-      };
-    });
-  };
-
-  addCategory = (category) => {
-    this.setState((prev) => ({
-      categoriesList: [...prev.categoriesList, category],
-    }));
-  };
-
-  render() {
-    const {
-      isOpenCatlList,
+    const transaction = {
       day,
       time,
       category,
-      currency,
       sum,
+      currency,
       comment,
-      categoriesList,
-    } = this.state;
-    const { transType, handleGoBack } = this.props;
-    console.log(categoriesList);
-    const categoryValue =
-      categoriesList.find(({ name }) => this.state.category === name)?.title ||
-      categoriesList[0].title;
+    };
+    toggleLoader();
+    addTransactionApi({ transaction, transType })
+      .then((transaction) => addTransaction({ transaction, transType }))
+      .catch((err) => setError(err.message))
+      .finally(() => toggleLoader());
+  };
 
-    return (
-      <>
-        <GoBackHeader
-          handleGoBack={!isOpenCatlList ? handleGoBack : this.toggleCatList}
-          title={
-            isOpenCatlList
-              ? "Категории"
-              : transType === "incomes"
-              ? "Доходы"
-              : "Расходы"
-          }
-        />
-        {!isOpenCatlList ? (
-          <form onSubmit={this.handleSubmit}>
-            <Button type="submit" title="Ok" />{" "}
-            {this.state.isLoading && (
-              <Loader
-                type="Puff"
-                color="#00BFFF"
-                height={30}
-                width={30}
-                timeout={3000} //3 secs
-              />
-            )}
-            <LabelInput
-              value={day}
-              title={"День"}
-              name={"day"}
-              type={"date"}
-              cbOnChange={this.handleChange}
-            />
-            <LabelInput
-              value={time}
-              title={"Время"}
-              name={"time"}
-              type={"time"}
-              cbOnChange={this.handleChange}
-            />
-            <LabelInput
-              title={"Категория"}
-              name={"category"}
-              type={"button"}
-              value={categoryValue}
-              cbOnClick={this.toggleCatList}
-            />
-            <LabelInput
-              title={"Сумма"}
-              name={"sum"}
-              placeholder={"Введите сумму"}
-              value={sum}
-              cbOnChange={this.handleChange}
-            />
-            <LabelInput
-              title={"Валюта"}
-              name={"currency"}
-              type={"button"}
-              value={currency}
-            />
-            <LabelInput
-              value={comment}
-              name={"comment"}
-              placeholder={"Комментарий"}
-              cbOnChange={this.handleChange}
-            />
-          </form>
-        ) : (
-          <CategoriesList
-            categoriesList={categoriesList}
-            handleChange={this.handleChange}
-            currCategory={this.state.category}
-            deleteCategory={this.deleteCategory}
-            addCategory={this.addCategory}
-          />
-        )}
-      </>
+  const deleteCategory = (id) =>
+    setCategoriesList((prev) =>
+      prev.filter((category) => category.name !== id)
     );
-  }
-}
 
-// TransactionPage.propTypes = {
-//   transType: PropTypes.string.isRequired,
-//   handleGoBack: PropTypes.func.isRequired,
-//   addTransaction: PropTypes.func.isRequired,
-// };
+  const addCategory = (category) =>
+    setCategoriesList((prev) => [...prev, category]);
+
+  const categoryValue =
+    categoriesList.find(({ name }) => category === name)?.title ||
+    categoriesList[0].title;
+
+  return (
+    <>
+      <GoBackHeader
+        handleGoBack={!isOpenCatList ? handleGoBack : toggleCatList}
+        title={
+          isOpenCatList
+            ? "Категории"
+            : transType === "incomes"
+            ? "Доходы"
+            : "Расходы"
+        }
+      />
+      {!isOpenCatList ? (
+        <form onSubmit={handleSubmit}>
+          <Button type="submit" title="Ok" />{" "}
+          {isLoading && (
+            <Loader
+              type="Puff"
+              color="#00BFFF"
+              height={30}
+              width={30}
+              timeout={3000} //3 secs
+            />
+          )}
+          <LabelInput
+            value={day}
+            title={"День"}
+            name={"day"}
+            type={"date"}
+            cbOnChange={handleChange}
+          />
+          <LabelInput
+            value={time}
+            title={"Время"}
+            name={"time"}
+            type={"time"}
+            cbOnChange={handleChange}
+          />
+          <LabelInput
+            title={"Категория"}
+            name={"category"}
+            type={"button"}
+            value={categoryValue}
+            cbOnClick={toggleCatList}
+          />
+          <LabelInput
+            title={"Сумма"}
+            name={"sum"}
+            placeholder={"Введите сумму"}
+            value={sum}
+            cbOnChange={handleChange}
+          />
+          <LabelInput
+            title={"Валюта"}
+            name={"currency"}
+            type={"button"}
+            value={currency}
+          />
+          <LabelInput
+            value={comment}
+            name={"comment"}
+            placeholder={"Комментарий"}
+            cbOnChange={handleChange}
+          />
+        </form>
+      ) : (
+        <CategoriesList
+          categoriesList={categoriesList}
+          handleChange={handleChange}
+          currCategory={category}
+          deleteCategory={deleteCategory}
+          addCategory={addCategory}
+        />
+      )}
+    </>
+  );
+};
+
+TransactionPage.propTypes = {
+  transType: PropTypes.string.isRequired,
+  handleGoBack: PropTypes.func.isRequired,
+  addTransaction: PropTypes.func.isRequired,
+};
 
 export default TransactionPage;
